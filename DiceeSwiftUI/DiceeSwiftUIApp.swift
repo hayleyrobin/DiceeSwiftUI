2dbee1977d3926eccba50492f692e2edc46b00f4
Hayley Robinson <hayley.robinson63@myhunter.cuny.edu>
2022-01-24 18:51:46 -0500

    Initial Commit

---
+++ DiceeSwiftUI/DiceeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeSwiftUIApp.swift
+//  DiceeSwiftUI
+//
+//  Created by Hayley Robinson on 1/24/22.
+//
+
+import SwiftUI
+
+@main
+struct DiceeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
